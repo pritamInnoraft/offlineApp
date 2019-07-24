755484bb6f0a8b86a6d42567b8905c80be02ddd9
Pritam Sadhu <pritam.sadhu@innoraft.com>
2019-07-24 16:03:41 +0530

    secured persisted key

--- src/store/store.js
+++ src/store/store.js
@@ -4,10 +4,19 @@ import storage from 'redux-persist/lib/storage' // defaults to localStorage for 
 import { composeWithDevTools } from "redux-devtools-extension";
 import ReduxThunk from "redux-thunk"
 import rootReducer from "./reducers";
+import createEncryptor from 'redux-persist-transform-encrypt'
+
+
+const encryptor = createEncryptor({
+    secretKey: 'my-super-secret-key',
+})
 
 const persistConfig = {
     key: 'root',
     storage,
+    transforms: [
+        encryptor
+    ]
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
